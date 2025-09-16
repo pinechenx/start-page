3dde3272084a496a0af030bead9e1bd539441c19
chen <1658086552@qq.com>
2025-09-16 17:24:27 +0800

    fix: 修复图片显示错误

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -1,13 +1,18 @@
+import googleicon from '@/assets/google.svg'
+import bingicon from '@/assets/bing.svg'
+import baiduicon from '@/assets/baidu.svg'
+import sogouicon from '@/assets/sogou.svg'
+
 export default {
   // 作者
   author: '流明灵石',
 
   // 搜索引擎
   searchEngineList: [
-    { name: '谷歌', url: 'https://www.google.com/search?q=', icon: '/src/assets/google.svg', shortcut: 'Alt + 1' },
-    { name: '必应', url: 'https://www.bing.com/search?q=', icon: '/src/assets/bing.svg', shortcut: 'Alt + 2' },
-    { name: '百度', url: 'https://www.baidu.com/s?wd=', icon: '/src/assets/baidu.svg', shortcut: 'Alt + 3' },
-    { name: '搜狗', url: 'https://www.sogou.com/web?query=', icon: '/src/assets/sogou.svg', shortcut: 'Alt + 4' },
+    { name: '谷歌', url: 'https://www.google.com/search?q=', icon: googleicon, shortcut: 'Alt + 1' },
+    { name: '必应', url: 'https://www.bing.com/search?q=', icon: bingicon, shortcut: 'Alt + 2' },
+    { name: '百度', url: 'https://www.baidu.com/s?wd=', icon: baiduicon, shortcut: 'Alt + 3' },
+    { name: '搜狗', url: 'https://www.sogou.com/web?query=', icon: sogouicon, shortcut: 'Alt + 4' },
   ],
 
   // 备案信息 为空则不显示
